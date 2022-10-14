957e2608e8a1a0cb1ef33eb645dfd448071013fa
Nina Hawari <nina.hawari@gmail.com>
2022-10-14 22:58:05 +0300

    new edit

--- tailwindConfig.js
+++ tailwindConfig.js
@@ -4,24 +4,34 @@ tailwind.config = {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
         noto: ["Noto-Sans", "mono"],
+        arapey: ["Arapey", "san serif"],
       },
+      fontSize: {
+        '8xl': '15rem',
+        '9xl': '16rem',
     },
 
+    },
+    transitionProperty: {
+      'height': 'height',
+      'spacing': 'margin, padding',
+    },
       colors: {
         y: "#EEEA89",
         p: "#AB72BF",
         b: "#343434",
         w: "#F2F2F2",
+
       },
 
-      backgroundImage: {
-        overlay: "url(./nordwood-themes-R53t-Tg6J4c-unsplash.jpg')",
-      },
+      // backgroundImage: {
+      //   overlay: "url(./nordwood-themes-R53t-Tg6J4c-unsplash.jpg')",
+      // },
 
       animation: {
         bounceLow: "new_bounce 1s ease-in-out infinite alternate",
 
-        blob: "new_blob 7s infinite",
+        blob: "new_blob 10s ease-in-out infinite",
         
       },
       
@@ -42,10 +52,10 @@ tailwind.config = {
              
                 },
                "33%": {
-                  transform: "translate(30px, -50px) scale(1.1)",
+                  transform: "translate(5px, -100px) scale(1.1) ",
                   },
                "66%": {
-                  transform: "translate(20px, -20px) scale(1)",
+                  transform: "translate(-50px, 200px) scale(1)",
                   },
 
                 "100%": {
@@ -54,6 +64,8 @@ tailwind.config = {
   
             }
           },
-      
+     
         },
+
+    
       }
